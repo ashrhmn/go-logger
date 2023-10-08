f4b16c6c72bffdc5a8384b26264554c5c2e7b034
Ashik Rahman <ashrhmn99@gmail.com>
2023-10-09 01:26:51 +0600

    rmq config

--- app.go
+++ app.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/ashrhmn/go-logger/consumer"
 	"github.com/ashrhmn/go-logger/modules"
 	"github.com/ashrhmn/go-logger/server"
@@ -8,19 +10,27 @@ import (
 	"go.uber.org/fx"
 )
 
+func ConstructQueueConfig() types.QueueConfig {
+	queueName := os.Getenv("QUEUE_NAME")
+	if queueName == "" {
+		queueName = "LogQueue"
+	}
+	rmqUrl := os.Getenv("RMQ_URL")
+	if rmqUrl == "" {
+		rmqUrl = "amqp://guest:guest@localhost:5672/"
+	}
+	return types.QueueConfig{
+		QueueName: queueName,
+		Url:       rmqUrl,
+	}
+}
+
 func main() {
 	fx.New(
 		server.Module,
 		consumer.Module,
 		modules.Register,
 		fx.NopLogger,
-		fx.Provide(
-			func() types.QueueConfig {
-				return types.QueueConfig{
-					QueueName: "LogQueue",
-					Url:       "amqp://guest:guest@localhost:5672/",
-				}
-			},
-		),
+		fx.Provide(ConstructQueueConfig),
 	).Run()
 }
